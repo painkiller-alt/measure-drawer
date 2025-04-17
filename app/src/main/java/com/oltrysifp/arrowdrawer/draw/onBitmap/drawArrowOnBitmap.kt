91447f3ccd4426b49fb3dea97b3b2ce44ca19dc2
Oltry <dimkaalex7@gmail.com>
2025-04-17 12:01:14 +0300

    quickfix

--- app/src/main/java/com/oltrysifp/arrowdrawer/draw/onBitmap/drawArrowOnBitmap.kt
+++ app/src/main/java/com/oltrysifp/arrowdrawer/draw/onBitmap/drawArrowOnBitmap.kt
@@ -30,7 +30,7 @@ fun drawArrowOnBitmap(
 
         canvas.drawLine(start.x, start.y, end.x, end.y, paint)
 
-        val arrowHeadSize = 30f
+        val arrowHeadSize = (6f*line.thickness).coerceIn(10f, 100f)
 
         val arrowAngle = Math.toRadians(30.0) // 30-degree arrowhead angle
         val angle = atan2(end.y - start.y, end.x - start.x)
@@ -53,21 +53,23 @@ fun drawArrowOnBitmap(
             start.y + arrowHeadSize * sin(angle + arrowAngle).toFloat()
         )
 
-        val pathEnd = Path().apply {
-            moveTo(end.x, end.y)
-            lineTo(endArrowPoint1.x, endArrowPoint1.y)
-            moveTo(end.x, end.y)
-            lineTo(endArrowPoint2.x, endArrowPoint2.y)
-        }
+        val xShift = 0.25f*line.thickness
+
         val pathStart = Path().apply {
-            moveTo(start.x, start.y)
+            moveTo(start.x+xShift, start.y)
             lineTo(startArrowPoint1.x, startArrowPoint1.y)
-            moveTo(start.x, start.y)
+            moveTo(start.x+xShift, start.y)
             lineTo(startArrowPoint2.x, startArrowPoint2.y)
         }
-
-        canvas.drawPath(pathEnd, paint)
         canvas.drawPath(pathStart, paint)
+
+        val pathEnd = Path().apply {
+            moveTo(end.x-xShift, end.y)
+            lineTo(endArrowPoint1.x, endArrowPoint1.y)
+            moveTo(end.x-xShift, end.y)
+            lineTo(endArrowPoint2.x, endArrowPoint2.y)
+        }
+        canvas.drawPath(pathEnd, paint)
     }
 
     return newBitmap
